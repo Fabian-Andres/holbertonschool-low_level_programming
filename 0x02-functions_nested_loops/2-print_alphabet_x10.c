08971fd03ce9223fe2dae08c3e0281536f01ddd1
Fabian-Andres <f4bian.andres@gmail.com>
2020-02-11 16:13:35 +0000

    Answer 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+* print_alphabet_x10 - Print alphabet in for function
+*
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+	char i;
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+
+		_putchar('\n');
+	}
+
+}
