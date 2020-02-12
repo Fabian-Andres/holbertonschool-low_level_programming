ddb81b07b7a7be2d1e90192b65cc6d67d4f56d64
Fabian-Andres <f4bian.andres@gmail.com>
2020-02-12 04:03:25 +0000

    Answer for question 7

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+* add - Print sum
+*
+*/
+int add(int a, int b)
+{
+	int res;
+
+	res = a + b;
+
+	return (res);
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -8,4 +8,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
+void jack_bauer(void);
+/* 10 */
+int add(int, int);
 #endif
