e48c4dd8d87d08e3cd67d82aac14bdb72c2a65c1
Fabian-Andres <f4bian.andres@gmail.com>
2020-02-13 23:54:49 +0000

    Answer 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main - function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 )
+		{
+			if (n % 5)
+			{
+				printf("Fizz ");
+			}
+			else
+			{
+
+				printf("FizzBuzz ");
+			}
+		}
+		else if (n % 5 == 0)
+		{			
+			printf("Buzz ");
+		}
+		else {
+			printf("%i ", n);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
