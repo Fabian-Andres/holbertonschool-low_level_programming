8d9873ddededa82db15de0ebbebbeff2a2a5e2b6
Fabian-Andres <f4bian.andres@gmail.com>
2020-02-13 22:43:54 +0000

    Answer 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * print_diagonal - function
+ *
+ * @n: number
+ * Return: Always 0 (Success)
+ */
+void print_diagonal(int n)
+{
+	int line, row;
+
+	for (line = 0; line < n; line++)
+	{
+		
+		for (row = 0; row < line; row++)
+		{
+			
+			if (n > 0)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+		_putchar(92);
+		_putchar('\n');
+	}	
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}		
+}
