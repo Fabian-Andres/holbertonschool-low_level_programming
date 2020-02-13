c2d9814fe703d69ecd08bd639a57bbf6849f3a9d
Fabian-Andres <f4bian.andres@gmail.com>
2020-02-13 16:28:21 +0000

    Answer for 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * mul - function to multiply
+ *
+ * @c: Character
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int res;
+	res = a * b;
+	return (res);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -2,4 +2,5 @@
 #define HOLBERTON
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 #endif
