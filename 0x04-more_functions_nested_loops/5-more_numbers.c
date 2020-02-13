348d0cee48959153c8696b7ace0246efaaafc369
Fabian-Andres <f4bian.andres@gmail.com>
2020-02-13 19:51:02 +0000

    Answer 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * more_numbers - function to multiply
+ *
+ * Return: Always 0 (Success)
+ */
+void more_numbers(void)
+{
+	int i, n;
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i >= 9)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
