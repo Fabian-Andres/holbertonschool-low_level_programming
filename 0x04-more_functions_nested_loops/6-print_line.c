64b5b38f58fbdd32703d7e7bfb0532e7eeb800b5
Fabian-Andres <f4bian.andres@gmail.com>
2020-02-13 20:43:31 +0000

    Answer 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_line - function to multiply
+ *
+ * @n: number
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; ++i)
+	{
+		if (n != 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
