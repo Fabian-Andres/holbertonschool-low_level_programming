51c5b63ad2f1b57b66189ede9e21113db98789ad
Fabian Andres <f4bian.andres@gmail.com>
2020-02-18 16:07:43 +0000

    Answer 0

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+	#include "holberton.h"
 /**
 * print_alphabet - Print alphabet in for function
 *
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* reset_to_98 - function
+*
+* Return: Always 0.
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
