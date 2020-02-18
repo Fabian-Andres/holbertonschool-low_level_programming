12ea392dc461c188250bd6d0495d390c12d2fe27
Fabian Andres <f4bian.andres@gmail.com>
2020-02-18 21:58:26 +0000

    Answer 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+/**
+ * rev_string - Function.
+ *
+ * @s: Value s
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int p, i, n = 0;
+	char new_arr[10];
+
+	for (p = 0; s[p] != '\0'; p++)
+	{
+	}
+
+	for (i = (p - 1); s[i] > 0; i--)
+	{
+		new_arr[n++] = s[i];
+	}
+
+	for (p = 0; s[p] != '\0'; p++)
+	{
+		s[p] = new_arr[p];
+	}
+	new_arr[n++] = s['\0'];
+}
