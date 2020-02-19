b6913c3c69c00de6fbe8eff893a9593885656e0b
Fabian-Andres <f4bian.andres@gmail.com>
2020-02-19 04:44:14 +0000

    Answer 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * *_strcpy - Function.
+ *
+ * @dest: Value string
+ * @src: Value source
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i, n;
+
+	for (n = 0; src[n] != '\0'; n++)
+		if (src[n] == '\0')
+			printf("\n");
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return dest;
+
+}
