7d2303621c92e1ef1e05d8ce83545855df2f8750
Fabian Andres <f4bian.andres@gmail.com>
2020-02-24 18:42:42 +0000

    Answer 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * *_strchr - Function.
+ *
+ * @s: Value
+ * @c: Value
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int i /** Index */;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return (NULL);
+}
