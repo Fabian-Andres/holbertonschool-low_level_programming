91269ed1ccf906990867710017e254376c450bf3
Fabian Andres <f4bian.andres@gmail.com>
2020-02-26 21:26:22 +0000

    Answer 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - Function puts
+ *
+ * @s: Pointer od array
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
