d489324c48c017da1a43883d3c47db401237abd0
Fabian-Andres <f4bian.andres@gmail.com>
2020-02-28 04:59:43 +0000

    Answer 7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
-
+#include <string.h>
 /**
  * _print_rev_recursion - Function puts
  *
  * @s: Pointer od array
  * Return: Always 0.
  */
+
+int _compare(char *s, int x, int y);
+int _strlen_recursion(char *s);
+
 int is_palindrome(char *s)
 {
-	char a, b;
+	int x = 0, y;
 
-	if (*s != '\0')
+	y = _strlen_recursion(s);
+
+	return (_compare(s, x, y));
+}
+
+int _compare(char *s, int x, int y)
+{
+	if (x >= y)
 	{
-		a = *s;
-		is_palindrome(s + 1);
-		b = *s;
-		printf("s - %c, ab (%c %c) \n", *s, a, b );
-		if (a == b)
-		{
-			return (1);
-		}
+		if (s[x] == s[y])
+			_compare(s, (x + 1), (y - 1));
 		else
 			return (0);
+	}
+
+	return (1);
+}
+/**
+ * _strlen_recursion - Function puts
+ *
+ * @s: Pointer od array
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] != '\0')
+	{
+		s++;
+		return (_strlen_recursion(s) + 1);
 	}
 	return (0);
 }
