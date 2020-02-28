eacec1ab4257b5f8d5e66c613adaf6d607b55666
Fabian-Andres <f4bian.andres@gmail.com>
2020-02-28 03:44:08 +0000

    Answer 8

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * wildcmp - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) != '\0' && *s1 == '\0')
+			return (0);
+		else
+			return (wildcmp(s1, (s2 +1)) ||  wildcmp((s1 + 1), s2));
+	}
+
+	if (*s2 == *s1)
+		return (wildcmp(s1 + 1, s2 + 1));
+
+	return (0);
+}
