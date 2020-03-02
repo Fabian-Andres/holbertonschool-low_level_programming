c0f39c2f7dd11d5ad87f42257dd4a0619cd877d7
Fabian-Andres <f4bian.andres@gmail.com>
2020-03-02 04:32:55 +0000

    Answer 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 /**
  * main - Function
  *
@@ -10,19 +9,40 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, money, change;
+	int coin[] = {25, 10, 5, 2, 1};
 
-	for (i = 1; i < argc; i++)
+	change = 0;
+	money = atoi(argv[1]);
+
+	if (argc != 2)
 	{
-		if (!isdigit(*argv[i]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-			sum += atoi(argv[i]);
+		printf("Error\n");
+		return (1);
 	}
-	printf("%d\n", sum);
-
+	if (money < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	while (money != 0)
+	{
+		for (i = 0; i <= 5; i++)
+		{
+			if (money > coin[i])
+			{
+				money -= coin[i];
+				change++;
+				break;
+			}
+			if ((money % coin[i]) == 0)
+			{
+				money -= coin[i];
+				change++;
+				break;
+			}
+		}
+	}
+	printf("%d\n", change);
 	return (0);
 }
