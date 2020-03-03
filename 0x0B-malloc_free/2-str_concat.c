803828b3661cb8b3cd384b85cc75f079875d2226
Fabian Andres <f4bian.andres@gmail.com>
2020-03-03 19:53:49 +0000

    Answer 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * sizestr - return size number
+ *
+ * @str: charactter to print
+ *
+ * Return: 0
+ */
+
+int sizestr(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+		;
+
+	return (i);
+}
+
+/**
+ * str_concat - print array of characters
+ *
+ * @s1: charactter to print
+ * @s2: charactter to print
+ *
+ * Return: 0
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, y, size1, size2;
+	char *new_arr;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (NULL);
+	}
+	size1 = sizestr(s1);
+	size2 = sizestr(s2) + 1;
+	new_arr = malloc(sizeof(char) * (size1 + size2));
+
+	if (new_arr == NULL)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < size1)
+	{
+		new_arr[i] = s1[i];
+		i++;
+	}
+
+	y = 0;
+	while (y < size2)
+	{
+		new_arr[i] = s2[y];
+		i++;
+		y++;
+	}
+	new_arr[i] = '\0';
+	return (new_arr);
+}
