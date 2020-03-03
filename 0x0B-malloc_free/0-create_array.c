5c1b9d94dd05853caf4d5216dc0f09eb2739d2ea
Fabian Andres <f4bian.andres@gmail.com>
2020-03-03 16:00:18 +0000

    Answer 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * create_array - print array of characters
+ *
+ * @size: the size of the memory to print
+ * @c: charactter to print
+ *
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *new_arr;
+
+	new_arr = malloc(sizeof(char) * size);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		new_arr[i] = c;
+		i++;
+	}
+	new_arr[i] = '\0';
+	return (new_arr);
+}
