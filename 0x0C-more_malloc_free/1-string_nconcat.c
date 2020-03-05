4b8145383d37adc3c2234d9c6ae98da1db29038d
Fabian Andres <f4bian.andres@gmail.com>
2020-03-05 19:42:23 +0000

    Update exc and betty 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,9 +31,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, y;
 	unsigned int size1, size2;
 
-	if (s1 == NULL || s2 == NULL)
-		return ("");
-
 	size1 = longarr(s1);
 	size2 = n;
 
