4d9ce59cd7907c4d0764871752e5f0dcd9769f4c
Fabian Andres <f4bian.andres@gmail.com>
2020-03-05 19:37:21 +0000

    Update exc and betty 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * string_nconcat - Funtion string_nconcat
+ * longarr - Funtion longarr
  *
+ * @s1: value arr
  * Return: Always 0.
  */
 int longarr(char *s1)
 {
 	int i = 0;
 
-	while (s1[i])
+	while (s1 && s1[i])
 	{
-		i++;	
+		i++;
 	}
 	return (i);
 }
 
+/**
+ * string_nconcat - Funtion string_nconcat
+ *
+ * @s1: value
+ * @s2: value
+ * @n: value
+ * Return: Always 0.
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_arr;
@@ -30,23 +39,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n >= size2)
 		size2 = longarr(s2);
-		
+
 	new_arr = malloc(sizeof(char) * (size1 + size2) + 1);
 	if (new_arr == NULL)
 	{
 		return (NULL);
 	}
 
-	for (i = 0; s1[i]; i++)
+	for (i = 0; s1 && s1[i]; i++)
 	{
-		new_arr[i] = s1[i];	
+		new_arr[i] = s1[i];
 	}
-	for (y = 0; y < n; y++)
+	for (y = 0; s2[y] && y < n; y++)
 	{
 		new_arr[i] = s2[y];
 		i++;
 	}
 	new_arr[i] = '\0';
 
-	 return (new_arr);
+	return (new_arr);
 }
