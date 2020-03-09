73aeebfae9090e59be1c5c0172d3b162e33bad56
Fabian Andres <f4bian.andres@gmail.com>
2020-03-09 21:41:35 +0000

    Update betty 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,15 @@
 #ifndef HOLBERTON
 #define HOLBERTON
+/**
+ * struct dog - dog structure
+ *
+ * @name: Name
+ * @age: age
+ * @owner: owner
+ */
 struct dog
 {
- 	char *name;
+	char *name;
 	float age;
 	char *owner;
 };
