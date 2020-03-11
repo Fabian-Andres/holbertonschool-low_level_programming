3f22c9702ec3347325b883c5e5d468924d6c7366
Fabian-Andres <f4bian.andres@gmail.com>
2020-03-11 04:11:36 +0000

    Answer 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free_dog
+ *
+	 * Return: Always 0.
+ */
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
