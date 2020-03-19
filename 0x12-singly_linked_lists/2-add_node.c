d72e8c16f4ee41c4b03d5f911c476a982cf7d383
Fabian-Andres <f4bian.andres@gmail.com>
2020-03-19 22:33:47 +0000

    Answer 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_node - add new node to linked list.
+ *
+ * @head: head of linked list
+ * @str: new string
+ * Return: list_t.
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	int i = 0;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	new_node->str = strdup(str);
+	new_node->len = i;
+	new_node->next = *head;
+
+	*head = new_node;
+
+	return (*head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LIST
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * struct list_s - structure of linked list.
  *
@@ -17,4 +18,5 @@ typedef struct list_s
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
