aef4acb1cc450d59d7d7d2d352d62407aba12385
Fabian-Andres <f4bian.andres@gmail.com>
2020-03-20 00:36:32 +0000

    Answer 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * add_node_end - add new node to linked list.
+ *
+ * @head: head of linked list
+ * @str: new string
+ * Return: list_t.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *current, *new_node;
+	int i = 0;
+
+	current = *head;
+
+	while (current->next != NULL)
+		current = current->next;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+		i++;
+
+	new_node->str = strdup(str);
+	new_node->len = i;
+	new_node->next = NULL;
+
+	current->next = new_node;
+
+	return (current);
+}
