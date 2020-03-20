6f1b865085a1e8152b53d1c7f5b4398e759a4de8
Fabian-Andres <f4bian.andres@gmail.com>
2020-03-20 03:07:39 +0000

    Answer advances

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+  
+#include "lists.h"
+
+/**
+ * phrase - print before
+ * Return: void
+ */
+void phrase(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
