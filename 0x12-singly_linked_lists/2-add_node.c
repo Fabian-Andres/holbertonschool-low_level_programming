22f1684282cf781a97b0d0b04befa6e7641dd518
Fabian-Andres <f4bian.andres@gmail.com>
2020-03-23 15:51:06 +0000

    Update exc

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,6 +14,11 @@ list_t *add_node(list_t **head, const char *str)
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
+		
+	if (head == NULL)
+	{
+		*head = new_node;
+	}
 
 	while (str[i] != '\0')
 		i++;
