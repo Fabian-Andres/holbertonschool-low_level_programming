1fa5a7821c9282750db2c09edbd6c32c1e358913
Fabian-Andres <f4bian.andres@gmail.com>
2020-03-24 21:19:41 +0000

    Answer 9

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - sum the value n into head.
+ *
+ * @head: head of list.
+ * @idx: index to add new node.
+ * @n: Value od new node.
+ * Return: value to sum.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new_node, *aux_head;
+	unsigned int i = 1;
+
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	if (head == NULL)
+	{
+		*head = new_node;
+	}
+
+	aux_head = *head;
+	while (i < idx)
+	{
+		if (aux_head == NULL)
+			return (NULL);
+		aux_head = aux_head->next;
+		i++;
+	}
+
+	new_node->n = n;
+	new_node->next = aux_head->next;
+
+	aux_head->next = new_node;
+
+	return (aux_head);
+}
