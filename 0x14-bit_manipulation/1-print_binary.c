886ff17837de01e1965147f6ed214ba19438cec0
Fabian-Andres <f4bian.andres@gmail.com>
2020-03-26 21:23:50 +0000

    Answer 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * print_binary - function convert int to binary.
+ *
+ * @n: number to convert.
+ * Return: Always 0.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int result = 0, aux_num = 0, aux_const = 1;
+	int i = 0, y = 0, j = 0, z;
+
+	if (n == 0)
+		_putchar('0');
+
+	aux_num = n;
+	while (aux_num != 0)
+	{
+		aux_num = aux_num >> aux_const;
+		i++;
+	}
+	z = i;
+	while (y < i)
+	{
+		aux_num = n;
+		while (j < z)
+		{
+			result = (aux_num & 1) + '0';
+			aux_num = aux_num >> 1;
+			j++;
+		}
+		_putchar(result);
+		result = 0;
+		z--;
+		y++;
+	}
+
+}
