b92858b4de05cac1be9e78d5882e6852d82c8b50
Fabian-Andres <f4bian.andres@gmail.com>
2020-03-26 21:39:49 +0000

    Update exc 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,31 +8,31 @@
  */
 void print_binary(unsigned long int n)
 {
-    unsigned int result = 0, aux_num = 0, aux_const = 1;
-	int i = 0, y = 0, j, z;
+	unsigned long int result = 0, aux_num = 0, aux_const = 1;
+	unsigned long int i = 0, y = 0, j, z;
 
 	if (n == 0)
 		_putchar('0');
 
-    aux_num = n;
+	aux_num = n;
 	while (aux_num != 0)
 	{
 		aux_num = aux_num >> aux_const;
 		i++;
 	}
-    z = i;
-    while (y < i)
-    {
-        aux_num = n;
-        for (j = 0; j < z; j++)
-        {
-            result = (aux_num & 1) + '0';
-            aux_num = aux_num >> 1;
-        }
-        _putchar(result);
-        result = 0;
-        z--;
-        y++;
-    }
+	z = i;
+	while (y < i)
+	{
+		aux_num = n;
+		result = 0;
+		for (j = 0; j < z; j++)
+		{
+			result = (aux_num & 1) + '0';
+			aux_num = aux_num >> 1;
+		}
+		_putchar(result);
+		z--;
+		y++;
+	}
 
 }
