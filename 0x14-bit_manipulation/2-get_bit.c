500bc42d22e9133ed49294faa68e9ccc4ff04ac4
Fabian-Andres <f4bian.andres@gmail.com>
2020-03-27 04:45:15 +0000

    Answer 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * get_bit - function get index bit.
+ *
+ * @index: position index to get.
+ * Return: Always 0.
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	return ((n >> index) & 1);
+}
